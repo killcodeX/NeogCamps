106166d046a72e0d96577902a314fb5466b88378
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-11-16 10:36:49 +0530

    tanay code done

---
+++ levelZero-Assignment1/TanayCode.js
@@ -0,0 +1,45 @@
+var readlineSync = require("readline-sync");
+
+var userName = readlineSync.question("What's your name ? ");
+
+console.log("Welcome " + userName + " to 'Do You Know Tanay?'" );
+
+var score = 0;
+
+// play function
+function play (question, answer){
+  var userAnswer = readlineSync.question(question);
+
+  if(userAnswer.toLowerCase() == answer.toLowerCase()){
+    console.log('Right :)')
+    score = score + 1;
+  } else {
+    console.log('Wrong :/')
+  }
+
+  console.log('Current Score : ', score);
+  console.log('-------------------------------')
+
+}
+
+var questions = [
+  {
+    question : 'Where do I live ? ',
+    answer : 'Bangalore'
+  },
+   {
+    question : 'My Favorite super hero would be ? ',
+    answer : 'Dhurv'
+  },
+   {
+    question : 'Where do I work ? ',
+    answer : 'Microsoft'
+  },
+];
+
+for(var i = 0; i < questions.length; i++){
+  var current = questions[i];
+  play(current.question, current.answer)
+}
+
+console.log('YAY your score :', score)
