da23006c0e4c2ae6035978f72307255136c45d3a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-11-30 14:22:35 +0530

    banana app working

---
+++ levelZero/assignment5/tanay's bhaya assignment/app.js
@@ -0,0 +1,36 @@
+// Api URL
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+
+// get variables
+var translateButton = document.querySelector(".btn");
+
+// read input
+var translateInput = document.querySelector("#translate-input");
+
+// show output
+var translateOutput = document.querySelector(".box-size");
+
+
+// event Handlers
+translateButton.addEventListener("click", buttonClickHandler)
+
+
+function buttonClickHandler(event) {
+    console.log("button clicked", translateInput.value);
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+    
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
